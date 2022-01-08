0636f1c04837c8db9249a06fa81d7fb5c58ca030
Baptiste Peupier <60870677+BaptistePeupier@users.noreply.github.com>
2022-01-08 12:12:39 +0100

    HerokuFix2

--- server.prod.js
+++ server.prod.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
 const path = require('path');
 const app = express();
 
 app.use(bodyParser.json())
+app.use(cookieParser());
 
 // Routes
 // For User
